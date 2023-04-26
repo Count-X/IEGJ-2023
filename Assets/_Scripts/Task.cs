5efa7301ad4199597ac73091d34c6e6e6c70e4ea
Count X <markus.s.linnanen@gmail.com>
2023-04-26 06:36:27 +0300

    Code & Stuff, Error prone
    This Code may/will have some errors as it is written without the assistance of the editor window as it was not co-operating on my computer. So in other words this code is not tested.

--- Assets/_Scripts/Task.cs
+++ Assets/_Scripts/Task.cs
@@ -3,32 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Task : MonoBehaviour
-{
+using TaskManager.taskStates = tState;
 
-    TaskManager.taskStates state;
-    
+public class Task : MonoBehaviour
+{   
+
+    tState state;
+    Transform player;
+
+    bool canFix;
+    float goodTime;
+    float BreakTime;
+
+    public float TimeToBreak = 3f;
+    public float TimeThatStaysGood = 5f;
+    public float DistanceLimit = 1f;
+
     public GameObject[] stateObjects;
 
-    public Slider progressSlider;
-    public Sprite alertSprite;
+    public BaseSlider<T0> progressSlider;
+    public GameObject AlertObject;
     
+    void Start(){
+        player = GameObject.FindWithTag("Player").transform;
+        ChangeState(TaskManager.taskStates.Good);
+        goodTime = TimeThatStaysGood;
+        progressSlider.highValue = TimeToBreak;
+    }
 
     // Update is called once per frame
     void Update()
     {
 
+
+
+        if( state != TaskManager.taskStates.Issue && Vector3.Distance(transform.position, player.position) <= DistanceLimit){
+            canFix = true;
+        }
+        else{
+            canFix = false;
+        }
+
+        if(canFix && Input.GetKeyDown(KeyCode.E)){
+            BreakTime += Time.deltaTime;
+            if(BreakTime >= TimeToFix){
+                ChangeState(TaskManager.taskStates.Good);
+                goodTime = TimeThatStaysGood;
+            }
+        }
+        else{
+            BreakTime -= Time.deltaTime;
+            
+        }
+
+        switch (state) {
+            case (TaskManager.taskStates)0:
+                AlertObject.SetActive(false);
+                goodTime -= Time.deltaTime;
+            break;
+
+            case (TaskManager.taskStates)1:
+                AlertObject.SetActive(true);
+                progressSlider.value = BreakTime;
+            break;
+
+            case (TaskManager.taskStates)2:
+                AlertObject.SetActive(false);
+            break;
+        }
     }
 
     public void ChangeState (TaskManager.taskStates newState) {
         state = newState;
-        switch (state) {
-            case (TaskManager.taskStates)0:
-                break;
-            case (TaskManager.taskStates)1:
-                break;
-            case (TaskManager.taskStates)2:
-                break;
-        }
     }
 }
