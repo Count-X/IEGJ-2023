9bc37813b3151186e26326d1d2a69b5f0f83d1d1
Count X <markus.s.linnanen@gmail.com>
2023-04-26 18:22:05 +0300

    Final Commit?
    thing that i hopefully works

--- Assets/_Scripts/Task.cs
+++ Assets/_Scripts/Task.cs
@@ -3,32 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-using TaskManager.taskStates = tState;
+
 
 public class Task : MonoBehaviour
 {   
 
-    tState state;
+    TaskManager.taskStates state;
     Transform player;
 
     bool canFix;
     float goodTime;
     float BreakTime;
-
+    
     public float TimeToBreak = 3f;
-    public float TimeThatStaysGood = 5f;
+    public float TimeThatStaysGoodMin = 5f;
+    public float TimeThatStaysGoodMax = 30f;
     public float DistanceLimit = 1f;
 
-    public GameObject[] stateObjects;
-
-    public BaseSlider<T0> progressSlider;
+    public Slider progressSlider;
     public GameObject AlertObject;
     
     void Start(){
         player = GameObject.FindWithTag("Player").transform;
         ChangeState(TaskManager.taskStates.Good);
-        goodTime = TimeThatStaysGood;
-        progressSlider.highValue = TimeToBreak;
+        goodTime = RandomizeTime();
+        BreakTime = TimeToBreak;
+        
     }
 
     // Update is called once per frame
@@ -36,44 +36,60 @@ public class Task : MonoBehaviour
     {
 
 
-
-        if( state != TaskManager.taskStates.Issue && Vector3.Distance(transform.position, player.position) <= DistanceLimit){
-            canFix = true;
-        }
-        else{
-            canFix = false;
-        }
-
-        if(canFix && Input.GetKeyDown(KeyCode.E)){
-            BreakTime += Time.deltaTime;
-            if(BreakTime >= TimeToFix){
-                ChangeState(TaskManager.taskStates.Good);
-                goodTime = TimeThatStaysGood;
-            }
-        }
-        else{
-            BreakTime -= Time.deltaTime;
-            
-        }
-
         switch (state) {
             case (TaskManager.taskStates)0:
                 AlertObject.SetActive(false);
+                if(goodTime <= 0f){
+                    ChangeState(TaskManager.taskStates.Issue);
+                    gameObject.GetComponent<AudioSource>().Play();
+                }
+                else{
                 goodTime -= Time.deltaTime;
+                }
+                Debug.Log("Hello");
             break;
 
             case (TaskManager.taskStates)1:
                 AlertObject.SetActive(true);
                 progressSlider.value = BreakTime;
+                if(Vector3.Distance(transform.position, player.position) >= DistanceLimit){
+                    canFix = true;
+                    Debug.Log("Canfix" + canFix);
+                }
+                else{
+                    canFix = false;
+                }
+
+                if(canFix && Input.GetKeyDown(KeyCode.E)){
+                    BreakTime += Time.deltaTime;
+                    if(BreakTime > TimeToBreak){
+                        ChangeState(TaskManager.taskStates.Good);
+                        goodTime = RandomizeTime();
+                        BreakTime = TimeToBreak;
+                    }
+                }
+                else{
+                    BreakTime -= Time.deltaTime;
+                    if(BreakTime <= 0f){
+                        ChangeState(TaskManager.taskStates.Broken);
+                    }
+                }
+                Debug.Log("Hello1");
             break;
 
             case (TaskManager.taskStates)2:
                 AlertObject.SetActive(false);
+                Destroy(AlertObject);
+                Destroy(this);
             break;
         }
     }
 
     public void ChangeState (TaskManager.taskStates newState) {
         state = newState;
+    }
+    
+    float RandomizeTime(){
+        return Random.Range(TimeThatStaysGoodMin, TimeThatStaysGoodMax);
     }
 }
